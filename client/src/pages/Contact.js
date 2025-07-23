79110a48174b088f6bb33b0b154a5cc8d72aa94d
prateek155 <prateekagrawal120@gmail.com>
2025-07-23 18:02:40 +0530

    Update Contact.js

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -28,13 +28,13 @@ const Contact = () => {
               <div className="contact-method mb-3">
                 <p className="d-flex align-items-center">
                   <BiMailSend size={24} className="me-3" style={{ color: "#007bff" }} />
-                  <span>Email: <a href="mailto:prateekagrawal589@gmail.com" className="text-decoration-none">unsupervisedlearnersclub@mitwpu.edu.in</a></span>
+                  <span>Email: <a href="mailto:unsupervisedlearnersclub@mitwpu.edu.in" className="text-decoration-none">unsupervisedlearnersclub@mitwpu.edu.in</a></span>
                 </p>
               </div>
               <div className="contact-method mb-3">
                 <p className="d-flex align-items-center">
                   <BiPhoneCall size={24} className="me-3" style={{ color: "#007bff" }} />
-                  <span>Phone: <a href="tel:+917627073230" className="text-decoration-none">7627073230</a></span>
+                  <span>Phone: <a href="tel:+917627073230,9175185122" className="text-decoration-none">7627073230</a></span>
                 </p>
               </div>
               <div className="text-center">
