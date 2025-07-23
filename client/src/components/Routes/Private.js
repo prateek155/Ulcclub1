687bc1c92d43c6df9399869e6b92e84473fa7491
prateek155 <prateekagrawal120@gmail.com>
2025-07-23 19:56:57 +0530

    Update Private.js

--- client/src/components/Routes/Private.js
+++ client/src/components/Routes/Private.js
@@ -12,7 +12,7 @@ export default function PrivateRoute() {
         const authCheck = async () => {
             try {
                 
-                const res = await axios.get("http://localhost:8080/api/v1/auth/user-auth", {
+                const res = await axios.get("https://ulcclub1.onrender.com/api/v1/auth/user-auth", {
                    
                 });
 
