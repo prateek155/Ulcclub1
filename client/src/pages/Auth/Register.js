51ad1e70ad229e79056ca6c5e978dac17390fab9
prateek155 <prateekagrawal120@gmail.com>
2025-07-23 17:56:56 +0530

    Update Register.js

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -35,7 +35,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(`http://localhost:8080/api/v1/auth/register`, {
+      const res = await axios.post(`https://ulcclub1.onrender.com/api/v1/auth/register`, {
         name,
         email,
         password,
