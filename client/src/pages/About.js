752f357d71d802263ad1055fad15b36daeeede29
prateek155 <prateekagrawal120@gmail.com>
2025-08-28 15:02:21 +0530

    Update About.js

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -11,22 +11,17 @@ const About = () => {
             <img 
               src="/image/Socsa.png" 
               alt="About Us" 
-            style={{ width: '700px', height: '400px', objectFit: 'contain' }}
+            style={{ width: '500px', height: '400px', objectFit: 'contain' }}
             />
           </div>
           <div className="col-md-6">
-            <h1 className="text-primary mb-4 text-center">ULC Club</h1>
+            <h1 className="text-primary mb-4 text-center">SOCSA</h1>
             <p className="fs-5 ">
               Welcome to our website! We are dedicated to providing always updated Information and services 
               tailored to meet your unique needs. With a commitment to excellence and a passion for 
               innovation, our team strives to deliver exceptional value and user satisfaction. 
               Explore our website and discover why we are the trusted choice for so many students.
             </p>
-            <div className="d-flex justify-content-center mt-4">
-               <NavLink to="/About-more"
-               className="btn btn-outline-primary btn-lg me-3">Learn More
-              </NavLink>
-            </div>
           </div>
         </div>
       </div>
