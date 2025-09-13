d6124ab00ab92c12ff50c75a9f09fb176eb7906e
prateek155 <prateekagrawal120@gmail.com>
2025-09-14 01:20:47 +0530

    Update lostModel.js

--- backend/models/lostModel.js
+++ backend/models/lostModel.js
@@ -1,1 +1,26 @@
+import mongoose  from  "mongoose";
 
+const lostSchema = new mongoose.Schema(
+    {
+    name:{
+        type:String,
+        required: [true, 'Event name is required'],
+    },
+    description:{
+        type:String,
+        required: [true, 'Event description is required'],
+    },
+    photo:{
+        data:Buffer,
+        contentType:String
+    },
+    venue: {
+    type: String,
+    required: [true, 'Venue is required'],
+    },
+   
+},
+  { timestamps: true }
+)
+
+export default mongoose.model("Losts", lostSchema);
