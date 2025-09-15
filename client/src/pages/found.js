f94d24c56d5d0859cecdc89bc206f1522d2c44cc
prateek155 <prateekagrawal120@gmail.com>
2025-09-15 13:42:58 +0530

    Update found.js

--- client/src/pages/found.js
+++ client/src/pages/found.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const GetAllItems = () => {
+const found = () => {
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -85,4 +85,4 @@ const GetAllItems = () => {
   );
 };
 
-export default GetAllItems;
+export default found;
