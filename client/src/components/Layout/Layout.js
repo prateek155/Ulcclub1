016a24864eb32dd5b585547a5ff229ff9af9335f
prateek155 <prateekagrawal120@gmail.com>
2025-12-16 20:33:18 +0530

    Update Layout.js

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,32 +1,44 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
 import { Helmet } from "react-helmet";
-import { Toaster } from 'react-hot-toast';
+import { Toaster } from "react-hot-toast";
 
 const Layout = ({
   children,
-  title = "Info app ",
-  description = "mern stack project",
-  keyword = "mern,react,node,mongodb",
-  author = "Prateekinfoyt"
+  title = "Info App",
+  description = "MERN stack project",
+  keyword = "mern, react, node, mongodb",
+  author = "Prateekinfoyt",
 }) => {
   return (
-    <div>
+    <>
       <Helmet>
         <meta charSet="utf-8" />
         <meta name="description" content={description} />
-        <meta name="keyword" content={keyword} />
+        <meta name="keywords" content={keyword} />
         <meta name="author" content={author} />
         <title>{title}</title>
       </Helmet>
+
+      {/* Header */}
       <Header />
-      <main style={{ minHeight: "70vh" }}>
-        <Toaster />
+
+      {/* Main Content */}
+      <main
+        style={{
+          minHeight: "calc(100vh - 120px)", // header + footer
+          width: "100%",
+          display: "block",
+        }}
+      >
+        <Toaster position="top-right" />
         {children}
       </main>
+
+      {/* Footer */}
       <Footer />
-    </div>
+    </>
   );
 };
 
